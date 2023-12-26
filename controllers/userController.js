0b0628dd61f118b47c4da813778910e1917484eb
RG-GitUser <rileygaffney3@icloud.com>
2023-12-26 13:55:03 -0400

    added logic for delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
-const User = require('../models/User'); //require User model
+const User = require('../models/User'); // require User model
 
 exports.getAllUsers = async (req, res) => {
   try {
-    const users = await User.find();         //find all users 
+    const users = await User.find();         // find all users 
     res.json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -18,9 +18,32 @@ exports.createUser = async (req, res) => {       // create a user
   }
 };
  
-exports.updateUser = async (req, res) => {        //logic to update user by ID
-  // Implement logic to update a user by ID
-};
+
+exports.updateUser = async (req, res) => {   // update user logic
+    const userId = req.params.id;
+  
+    try {
+      // Find the user by ID
+      const user = await User.findById(userId);
+  
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      // update user properties based on the request body
+      user.username = req.body.username || user.username;
+      user.email = req.body.email || user.email;
+    
+      // save the updated user
+      const updatedUser = await user.save();
+  
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+
 
 exports.deleteUser = async (req, res) => {       //logic to delete user by ID
   // Implement logic to delete a user by ID
